f29c4145a55f1c56e544f30236857380bd9c1a63
John Mclaren <john@johnscs.com>
2019-06-08 22:37:05 -0400

    use script directory for download

--- openvpn-install.bat
+++ openvpn-install.bat
@@ -4,8 +4,8 @@ echo Administrative permissions required. Detecting permissions...
 net session >nul 2>&1
 if %errorLevel% == 0 (
 	echo Success: Administrative permissions confirmed.
-	bitsadmin.exe /transfer "JobName" http://build.openvpn.net/downloads/releases/latest/openvpn-install-latest-stable.exe %userprofile%\Downloads\openvpn-install-latest-stable.exe
-	%userprofile%\Downloads\openvpn-install-latest-stable.exe /S /SELECT_SHORTCUTS=0 /SELECT_SERVICE=1 /SELECT_OPENVPNGUI=0
+	bitsadmin.exe /transfer "JobName" http://build.openvpn.net/downloads/releases/latest/openvpn-install-latest-stable.exe %~dp0openvpn-install-latest-stable.exe
+	%~dp0openvpn-install-latest-stable.exe /S /SELECT_SHORTCUTS=0 /SELECT_SERVICE=1 /SELECT_OPENVPNGUI=0
 	robocopy %~dp0 "C:\Program Files\OpenVPN\config" *.ovpn
 	net stop OpenVPNService
 	net start OpenVPNService 
