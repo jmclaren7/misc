9dee2db90a36b3b79ebb4d508b62184c1beecf7d
John Mclaren <john@johnscs.com>
2019-09-24 01:53:06 -0400

    Update domain-Info.ps1

--- domain-Info.ps1
+++ domain-Info.ps1
@@ -11,7 +11,7 @@ $UsersInactive = (Get-ADUser -Properties LastLogonDate -Filter { (Enabled -eq $t
 $UsersDisabled = (Get-ADUser -Filter {Enabled -eq $false}).count
 
 Write-Host " "
-Write-Host "==========================================================================================="
+Write-Host "==============================================================================="
 Write-Host "Computers.........."$Computers
 Write-Host "    Inactive (90).."$ComputersInactive
 Write-Host "    Workstations..."$ComputersWorkstations
@@ -25,6 +25,6 @@ Write-Host "======= Inactive Users (Last 90 Days) ======="
 Get-ADUser -Properties LastLogonDate -Filter { (Enabled -eq $true) -and (LastLogonTimeStamp -lt $Date) -or (Enabled -eq $true) -and (LastLogonDate -notlike '*')} | Format-Table Name, UserPrincipalName, LastLogonDate
 Write-Host "======= Inactive Computers (Last 90 Days) ======="
 Get-ADComputer -Filter {LastLogonDate -lt $Date} -Properties OperatingSystem, LastLogonDate | Format-Table Name, OperatingSystem, LastLogonDate
-Write-Host "==========================================================================================="
+Write-Host "==============================================================================="
 Write-Host " "
 Read-Host -Prompt "Press enter to continue"
